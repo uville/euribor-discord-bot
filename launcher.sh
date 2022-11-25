cc5d19ca0ca00ca6b058d07e304a43de8f0c356d
Ville Utti <ville.utti@gmail.com>
2022-11-25 20:12:55 +0200

    Fixed launcher

--- launcher.sh
+++ launcher.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # launcher.sh
 # navigate to home directory, then to this directory, then execute python script, then back home
 
 cd /
 cd /home/pi/git/euribor-discord-bot
-source ".venv/bin/activate"
-echo $VIRTUAL_ENV
+set -e
+source /home/pi/git/euribor-discord-bot/.venv/bin/activate
 sudo python bot.py
+deactivate
 cd /
